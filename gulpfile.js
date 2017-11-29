2f93a7abd89a007ec69cfe132aa17c04ccb5652f
Tobias Lambrecht <tobiasla@gmx.de>
2017-11-29 17:30:40 +0100

    gulp css aufgaben

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require ('gulp'),
-watch = require ('gulp-watch');
+watch = require ('gulp-watch'),
+postcss = require ('gulp-postcss')
+autoprefixer = require ('autoprefixer'),
+cssvars = require ('postcss-simple-vars'),
+nested = require ('postcss-nested');
 
 gulp.task('default', function() {
 console.log ('gulp task');
@@ -10,8 +14,10 @@ gulp.task('html', function() {
     });
 
 gulp.task('styles', function() {
-        console.log ('Imagine something useful');
-        });
+        return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+    });
 
 gulp.task ('watch', function() {
     watch('./app/index.html', function() {

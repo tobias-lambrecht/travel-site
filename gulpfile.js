7b1f4d4490a561fc6a19067bc035babc5c06a3b4
Tobias Lambrecht <tobiasla@gmx.de>
2017-11-30 13:13:19 +0100

    Header Teil 1

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ postcss = require ('gulp-postcss')
 autoprefixer = require ('autoprefixer'),
 cssvars = require ('postcss-simple-vars'),
 nested = require ('postcss-nested');
+cssImport = require ('postcss-import');
 
 gulp.task('default', function() {
 console.log ('gulp task');
@@ -15,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
         return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
     });
 
